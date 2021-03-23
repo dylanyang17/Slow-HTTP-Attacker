0671f8df3b8a8eee199a3baa0abf85680a61ec5c
yangyr17 <315629555@qq.com>
2021-03-24 01:28:16 +0800

    refactor: 重构 args_parser

---
+++ README.md
@@ -0,0 +1,8 @@
+# Slow-HTTP-Attacker
+
+将三种 slow HTTP 攻击简单地移植到了 python 上，包括 Header 型、POST 型、Read 型。
+
+参考 repo：
+
+* slowhttptest：参考该仓库功能进行开发，https://github.com/shekyan/slowhttptest
+* slowloris：Header 型，为 python 代码，以该代码为基础开发，https://github.com/gkbrk/slowloris
---
+++ args_parser.py
@@ -0,0 +1,116 @@
+import argparse
+import logging
+import socket
+import sys
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Slow HTTP Attacker, a tool providing three types of slow HTTP attack."
+    )
+    parser.add_argument("host", nargs="?", help="Host to perform stress test on")
+    parser.add_argument(
+        "-m", "--mode", default='header', help='Mode of attack. The supported options are "header", "post" and "read"',
+        type=str
+    )
+    parser.add_argument(
+        "-p", "--port", default=80, help="Port of webserver, usually 80", type=int
+    )
+    parser.add_argument(
+        "-s",
+        "--sockets",
+        default=150,
+        help="Number of sockets to use in the test",
+        type=int,
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="verbose",
+        action="store_true",
+        help="Increases logging",
+    )
+    parser.add_argument(
+        "-ua",
+        "--randuseragents",
+        dest="randuseragent",
+        action="store_true",
+        help="Randomizes user-agents with each request",
+    )
+    parser.add_argument(
+        "-x",
+        "--useproxy",
+        dest="useproxy",
+        action="store_true",
+        help="Use a SOCKS5 proxy for connecting",
+    )
+    parser.add_argument(
+        "--proxy-host", default="127.0.0.1", help="SOCKS5 proxy host"
+    )
+    parser.add_argument(
+        "--proxy-port", default="8080", help="SOCKS5 proxy port", type=int
+    )
+    parser.add_argument(
+        "--https",
+        dest="https",
+        action="store_true",
+        help="Use HTTPS for the requests",
+    )
+    parser.add_argument(
+        "--sleeptime",
+        dest="sleeptime",
+        default=15,
+        type=int,
+        help="Time to sleep between each header sent.",
+    )
+    parser.set_defaults(verbose=False)
+    parser.set_defaults(randuseragent=False)
+    parser.set_defaults(useproxy=False)
+    parser.set_defaults(https=False)
+
+    args = parser.parse_args()
+
+    if len(sys.argv) <= 1:
+        parser.print_help()
+        sys.exit(1)
+
+    args.mode = args.mode.lower()
+
+    if args.mode not in ['header', 'post', 'read']:
+        print('Unsupported mode. The supported modes are "header", "post" and "read".')
+        sys.exit(1)
+
+    if not args.host:
+        print("Host required!")
+        parser.print_help()
+        sys.exit(1)
+
+    if args.useproxy:
+        # Tries to import to external "socks" library
+        # and monkey patches socket.socket to connect over
+        # the proxy by default
+        try:
+            import socks
+
+            socks.setdefaultproxy(
+                socks.PROXY_TYPE_SOCKS5, args.proxy_host, args.proxy_port
+            )
+            socket.socket = socks.socksocket
+            logging.info("Using SOCKS5 proxy for connecting...")
+        except ImportError:
+            logging.error("Socks Proxy Library Not Available!")
+
+    if args.verbose:
+        logging.basicConfig(
+            format="[%(asctime)s] %(message)s",
+            datefmt="%d-%m-%Y %H:%M:%S",
+            level=logging.DEBUG,
+        )
+    else:
+        logging.basicConfig(
+            format="[%(asctime)s] %(message)s",
+            datefmt="%d-%m-%Y %H:%M:%S",
+            level=logging.INFO,
+        )
+
+    return args
