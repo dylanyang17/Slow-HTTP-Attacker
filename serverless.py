ab2a6f2fbc47feffb8fcf1d8d55056b0a923de74
yangyr17 <315629555@qq.com>
2021-04-02 01:37:00 +0800

    feat: 加入 aliyun 的 serverless 调用函数

--- serverless.py
+++ serverless.py
@@ -9,3 +9,11 @@ def tencent_run(event, context):
     args = process_args_dict(event)
     attacker = Attacker(*args)
     attacker.attack()
+
+
+def aliyun_run(event, context):
+    logging.info('Aliyun Serverless.')
+    logging.info(context)
+    args = process_args_dict(event)
+    attacker = Attacker(*args)
+    attacker.attack()
